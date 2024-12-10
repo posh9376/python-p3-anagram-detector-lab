e41e383e96377c9f63a5c1c071d09948ceebd6c4
Eric Richard <reric0802@gmail.com>
2024-12-10 13:28:10 +0300

    created an anagram code

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    def match(self, words):
+        return [word for word in words if sorted(word) == sorted(self.word)]
+
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
